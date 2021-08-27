53bb9f165a158c87c983bf9148dafbb3538ca794
Ramazan TÜFEKÇİ <ramazan.tufekci@outlook.com>
2021-08-27 10:47:02 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+      var urlList = window.location.pathname.split("/");
+      $("#liste li").each(function(index,element){
+        if(urlList[index].length==0)
+        {
+          $(this).children().attr("href","/");
+        }else if((urlList.length-1)==index){
+          $(this).children().attr("href","#");
+        }else{
+          var cUrl = "";
+          for(let numb=1;numb<=index;numb++){
+              cUrl +="/"+urlList[numb];
+          }
+          
+          $(this).children().attr("href",cUrl);
+        }
+        
+      });
+    });
